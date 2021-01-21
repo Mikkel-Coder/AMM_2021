750de263d981bc1bd2addf194e47bf8661482b58
Mikkel-coder <mikk804s@eucnordvest.dk>
2021-01-21 10:01:27 +0100

    form

--- asteroid.js
+++ asteroid.js
@@ -1,6 +1,7 @@
 function Asteroid() {
     this.pos = createVector(random(width), random(height))
     this.r = 50;
+    this.total = floor(random(5, 15));
 
     this.render = function(){
         push();
@@ -9,8 +10,8 @@ function Asteroid() {
         translate(this.pos.x, this.pos.y);
         //ellipse(0,0, this.r*2)
         beginShape();
-        for (var i = 0; i < 10; i++) {
-            var angle = map(i, 0, 10, 0, TWO_PI);
+        for (var i = 0; i < this.total; i++) {
+            var angle = map(i, 0, this.total, 0, TWO_PI);
             var x = this.r*cos(angle);
             var y = this.r*sin(angle); 
             vertex(x, y);
