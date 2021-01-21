fdebb9795eb88bcaa061c3ee5643275ca52efc72
Mikkel-coder <mikk804s@eucnordvest.dk>
2021-01-21 09:50:59 +0100

    3

--- asteroid.js
+++ asteroid.js
@@ -2,11 +2,13 @@ function Asteroid() {
     this.pos = createVector(random(width), random(height))
     this.r = 50;
 
-
     this.render = function(){
-
+        push();
+        stroke(255);
+        noFill();
         translate(this.pos.x, this.pos.y);
         ellipse(0,0, this.r*2)
+        pop();
     }
 
 }
