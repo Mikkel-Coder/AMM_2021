20c73a1b4a29675484ee5251b09b155832191d7a
Mikkel-coder <mikk804s@eucnordvest.dk>
2021-01-21 09:39:55 +0100

    test

--- asteroid.js
+++ asteroid.js
@@ -1,1 +1,12 @@
+function Asteroid() {
+    this.pos = createVector(random(width), random(height))
+    this.r = 50;
 
+
+    this.render = function(){
+
+        translate(this.pos.x, this.pos.y);
+        ellipse(0,0 this.r*2, )
+    }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 var ship;
+var asteroids = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ship = new Ship();
+  asteroids.push(new Asteroid());
 }
 
 function draw() {
@@ -11,6 +13,10 @@ function draw() {
   ship.turn();
   ship.update();
   ship.edges();
+
+  for (var i = 0; i < asteroids.length; i++) {
+    asteroids[i].render();
+  }
 }
 function keyReleased() {
   ship.setRotation(0);
