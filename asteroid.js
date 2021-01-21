c5bf4f9786cfb3122fb53a57fe7f82695f3ff1ed
Mikkel-coder <mikk804s@eucnordvest.dk>
2021-01-21 09:58:08 +0100

    ja tak

--- asteroid.js
+++ asteroid.js
@@ -7,7 +7,15 @@ function Asteroid() {
         stroke(255);
         noFill();
         translate(this.pos.x, this.pos.y);
-        ellipse(0,0, this.r*2)
+        //ellipse(0,0, this.r*2)
+        beginShape();
+        for (var i = 0; i < 10; i++) {
+            var angle = map(i, 0, 10, 0, TWO_PI);
+            var x = this.r*cos(angle);
+            var y = this.r*sin(angle); 
+            vertex(x, y);
+        }
+        endShape(CLOSE);
         pop();
     }
 
