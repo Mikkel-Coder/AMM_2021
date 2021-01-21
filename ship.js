6b632eddf95ddc0c806f34830468519ac73c5b4b
Mikkel-coder <71432330+Mikkel-coder@users.noreply.github.com>
2021-01-21 10:30:15 +0100

    Update ship.js

--- ship.js
+++ ship.js
@@ -1,6 +1,6 @@
 function Ship() {
   this.pos = createVector (width/2, height/2);
-  this.r = 10;
+  this.r = 20;
   this.heading = 0;
   this.rotation = 0;
   this.vel = createVector(0,0)
@@ -26,11 +26,13 @@ function Ship() {
   }
 
   this.render = function() {
+    push();
     translate(this.pos.x, this.pos.y);
     rotate(this.heading + PI/2);
     noFill();
     stroke(255)
     triangle (-this.r, this.r, this.r, this.r,0, -this.r)
+    pop();
   }
 
   this.edges = function() {
