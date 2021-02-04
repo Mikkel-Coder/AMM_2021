c27c2ef7ed0dc8b5a8f4c96de960feba84844261
Mikkel-coder_GAMING <mikk804s@eucnordvest.dk>
2021-02-04 09:58:44 +0100

    Comments

--- laser.js
+++ laser.js
@@ -1,4 +1,4 @@
-function Laser(spos, angle) {
+function Laser(spos, angle) { //Laserne posotion og heading og deres hastighed (vel)
     this.pos = createVector(spos.x, spos.y);
     this.vel = p5.Vector.fromAngle(angle);
     this.vel.mult(10);
@@ -6,15 +6,15 @@ function Laser(spos, angle) {
     this.update = function(){
         this.pos.add(this.vel);
     }
-    this.render = function(){
+    this.render = function(){ //grafikken på laserne
         push();
         stroke(255);
         strokeWeight(4);
         point(this.pos.x, this.pos.y);
-        pop();
+        pop(); //For ikke at læse funktionen konstant
     }
 
-    this.hits = function(asteroid){
+    this.hits = function(asteroid){ //Fortæller med snadt eller flask om en laser har ramt en astroide
         var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
         if (d < asteroid.r) {
            return true;
